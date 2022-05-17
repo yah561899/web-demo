8dffd8e81685631016e5e498ff9b24f64afefe42
yah561899 <yah561899@gmail.com>
2022-05-17 15:18:48 +0800

    update

---
+++ web-demo/src/components/List/Item/Archibus.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import archibus from './images/Archibus.png'
+
+const Archibus = () => {
+  return (
+    <div>
+        <span>
+            <p>Powered by our in-house</p>
+            <p>State of the art technoloies</p>
+            <img src={archibus} alt="" style={{height:"20%"}}/>
+        </span>
+    </div>
+  )
+}
+
+export default Archibus
--- web-demo/src/components/List/list.js
+++ web-demo/src/components/List/list.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Archibus from './Item/Archibus'
+import ISO from './Item/ISO'
+import Step from './Item/step'
 
 const List = () => {
   return (
-    <div>list</div>
+    <div>
+      <hr/>
+      <Step/>
+      <hr/>
+      <ISO/>
+      <hr/>
+      <Archibus/>
+      <hr/>
+    </div>
   )
 }
 
