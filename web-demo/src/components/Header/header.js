5297e5ec9919cfccbbc34c2e939dae0ee4c8c0ee
felff <a909henhen@gmail.com>
2022-05-18 09:31:53 +0800

    123

--- web-demo/src/components/Header/header.js
+++ web-demo/src/components/Header/header.js
@@ -2,131 +2,135 @@ import React, { Component } from 'react'
 import './header.css'
 import img1 from './img/DIF-WM-LOGO.png'
 import img2 from './img/goals.png'
+import img3 from './img/ISO.png'
+import img4 from './img/penguin.png'
 export default class Header extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row bg-light border py-2">
-          <div className="col-lg-4">
-            <ul className="nav">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  <span className="icon">
-                    <i className="fa-brands fa-facebook-square"></i>
-                  </span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  <span className="icon">
-                    <i className="fa-brands fa-twitter-square"></i>
-                  </span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  <span className="icon">
-                    <i className="fa-brands fa-instagram"></i>
-                  </span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  <span className="icon">
-                    <i className="fa-brands fa-youtube"></i>
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-lg-8">
-            <ul className="nav justify-content-end">
-              <li className="nav-item">
-                <a className="nav-link" href="#"><span className="text">News & Events.</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#"><span className="text">Contact</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#"><span className="text">FAQs.</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  <span className="icon">
-                    <i className="fa fa-solid fa-globe"></i>
-                  </span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <div className="dropdown">
-                  <button className="btn btn-light text" type="button" data-bs-toggle="dropdown">
-                    English
-                    <i className="fa-solid fa-square-caret-down ps-2"></i>
-                  </button>
-                  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a className="dropdown-item text" href="#">Chinese</a></li>
-                    <li><a className="dropdown-item text" href="#">Korean</a></li>
-                  </ul>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="row pt-5 align-items-end">
-          <div className="col-lg-4 ">
-            <img src={img1} className="img-fluid" />
-          </div>
-          <div className="col-lg-2">
-            <img src={img2} className="img" />
-          </div>
-          <div className="col-lg-5 offset-lg-1">
-            <ul className="nav justify-content-between ">
-              <li className="nav-item">
-                <div className="dropdown">
-                  <button className="btn text" type="button" data-bs-toggle="dropdown">
-                    Solutions
-                    <i className="fa-solid fa-square-caret-down ps-2"></i>
-                  </button>
-                  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a className="dropdown-item text" href="#">Chinese</a></li>
-                    <li><a className="dropdown-item text" href="#">Korean</a></li>
-                  </ul>
-                </div>
-              </li>
-              <li className="nav-item">
-                <div className="dropdown">
-                  <button className="btn text" type="button" data-bs-toggle="dropdown">
-                    Resources
-                    <i className="fa-solid fa-square-caret-down ps-2"></i>
-                  </button>
-                  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a className="dropdown-item text" href="#">Chinese</a></li>
-                    <li><a className="dropdown-item text" href="#">Korean</a></li>
-                  </ul>
-                </div>
-              </li>
-              <li className="nav-item">
-                <div className="dropdown">
-                  <button className="btn text" type="button" data-bs-toggle="dropdown">
-                    Case Studies
-                    <i className="fa-solid fa-square-caret-down "></i>
-                  </button>
-                  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a className="dropdown-item text" href="#">Chinese</a></li>
-                    <li><a className="dropdown-item text" href="#">Korean</a></li>
-                  </ul>
-                </div>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text" href="#">About Us</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+    {/* <!--nav--> */}
+    <div className="row bg-light border py-2">
+      <div className="col-lg-4">
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <span className="icon">
+                <i className="fa-brands fa-facebook-square"></i>
+              </span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <span className="icon">
+                <i className="fa-brands fa-twitter-square"></i>
+              </span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <span className="icon">
+                <i className="fa-brands fa-instagram"></i>
+              </span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <span className="icon">
+                <i className="fa-brands fa-youtube"></i>
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="col-lg-8">
+        <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <a className="nav-link" href="#"><span className="text">News & Events.</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#"><span className="text">Contact</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#"><span className="text">FAQs.</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <span className="icon">
+                <i className="fa fa-solid fa-globe"></i>
+              </span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <div className="dropdown">
+              <button className="btn btn-light text" type="button" data-bs-toggle="dropdown">
+                English
+                <i className="fa-solid fa-square-caret-down"></i>
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a className="dropdown-item text" href="#">Chinese</a></li>
+                <li><a className="dropdown-item text" href="#">Korean</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    {/* <!--header--> */}
+    <div className="row al py-5 align-items-end">
+      <div className="col-lg-4">
+        <img src={img1} className="img-fluid "/>
+      </div>
+      <div className="col-lg-2 align-self-start">
+        <img src={img2} className=" img"/>
+      </div>
+      <div className="col-lg-5 offset-lg-1">
+        <ul className="nav justify-content-between ">
+          <li className="nav-item">
+            <div className="dropdown">
+              <button className="btn text" type="button" data-bs-toggle="dropdown">
+                Solutions
+                <i className="fa-solid fa-square-caret-down"></i>
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a className="dropdown-item text" href="#">Chinese</a></li>
+                <li><a className="dropdown-item text" href="#">Korean</a></li>
+              </ul>
+            </div>
+          </li>
+          <li className="nav-item">
+            <div className="dropdown">
+              <button className="btn text" type="button" data-bs-toggle="dropdown">
+                Resources
+                <i className="fa-solid fa-square-caret-down"></i>
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a className="dropdown-item text" href="#">Chinese</a></li>
+                <li><a className="dropdown-item text" href="#">Korean</a></li>
+              </ul>
+            </div>
+          </li>
+          <li className="nav-item">
+            <div className="dropdown">
+              <button className="btn text" type="button" data-bs-toggle="dropdown">
+                Case Studies
+                <i className="fa-solid fa-square-caret-down"></i>
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a className="dropdown-item text" href="#">Chinese</a></li>
+                <li><a className="dropdown-item text" href="#">Korean</a></li>
+              </ul>
+            </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text" href="#">About Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
 
-        <div className="row pt-5">
+    <div className="row pt-5">
       <div className="col-lg-7">
         <p>
           <h1 className="text">ISO<small className="ps-2">SUSTAINABILITY</small></h1>
-          <h2>COMPLIANCE,REPORTING &amp CERTIIFICATION</h2>
+          <h2>COMPLIANCE,REPORTING & CERTIIFICATION</h2>
           <h3>SOLUTIONS FOR SMEs</h3>
         </p>
           <div className="row mt-3 align-items-end">
@@ -152,13 +156,13 @@ export default class Header extends Component {
           </div>
       </div>
       <div className="col-lg-1 align-self-end">
-        <img src="./penguin.png" className="img-fluid img3"/>
+        <img src={img4} className="img-fluid img3"/>
       </div>
       <div className="col-lg-4 ">
-          <img src="./ISO.png" className="img-fluid"/>
+          <img src={img3} className="img-fluid"/>
       </div>
     </div>
-      </div>
+  </div>
     )
   }
 }
